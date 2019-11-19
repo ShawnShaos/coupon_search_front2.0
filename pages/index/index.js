7610eb7d3b3a97174399b8e1cc2a05ca6fe09cd7
ly <958893303@qq.com>
2019-11-19 19:12:28 +0800

    价格排序

--- pages/index/index.js
+++ pages/index/index.js
@@ -63,8 +63,10 @@ Page({
     curPage: 1,
     isShowMap: true, //是否显示轮播图和爆款推荐页以及排序选项
     TabListCur: 0,
-    isShowSort:false, //是否显示销量价格排序导航
-    priceUp: false //价格排序设置，默认升序
+    isShowSort: false, //是否显示销量价格排序导航
+    priceUp: false, //价格排序设置，默认升序
+    sort_type:0, //排序规则
+    opt_id:0 //商品栏目id
   },
   onLoad() {
     var that = this;
@@ -116,23 +118,33 @@ Page({
       TabCur: e.currentTarget.dataset.id,
       scrollLeft: (e.currentTarget.dataset.id - 1) * 60,
       activeCategoryId: e.currentTarget.dataset.opt_id,
+      opt_id: e.currentTarget.dataset.opt_id, //分类栏目id
+      TabListCur:0,
       curPage: 1
     })
+  
 
     this.goodsSearch({
-      opt_id: e.currentTarget.dataset.opt_id, //分类栏目id
       page: 1
     })
 
   },
   goodsSearch: function(data) { //获取商品列表
+
+    data.sort_type = this.data.sort_type; //排序方式
+    data.opt_id = this.data.opt_id; //分类栏目id
+
     var that = this;
     if (data.opt_id == 0) { //热搜排行榜
+      that.setData({
+        isShowMap: true,
+        isShowSort: false
+      });
       this.topGoodsListQuery(data)
     } else {
-
       that.setData({
-        isShowMap: false
+        isShowMap: false,
+        isShowSort: true
       });
 
       api.GoodsSearch({
@@ -144,8 +156,8 @@ Page({
           })
           return;
         }
-
-        if (data.page == 1) { //第一页
+        
+        if (that.data.curPage == 1) { //第一页
           that.setData({
             goodsRecommend: data.goods_search_response.goods_list,
           })
@@ -159,6 +171,7 @@ Page({
     };
   },
   tabListSelect(e) {
+    var that = this;
     this.setData({
       TabListCur: e.currentTarget.dataset.id,
     });
@@ -172,7 +185,29 @@ Page({
           priceUp: true
         })
       }
-    }
+    };
+
+    var sort_type = 0; //默认综合排序16
+    if (e.currentTarget.dataset.id == 1) {  //好评
+      sort_type = 16;
+    } else if (e.currentTarget.dataset.id == 2){  //销量降序
+      sort_type = 6;
+    } else if (e.currentTarget.dataset.id == 3 && this.data.priceUp) {  //价格升序
+      sort_type = 3;
+    } else if (e.currentTarget.dataset.id == 3 && !this.data.priceUp) {  //价格降序
+      sort_type = 4;
+    };
+
+    this.setData({
+      sort_type: sort_type
+    })
+
+    this.goodsSearch({
+      opt_id: that.data.opt_id, //分类栏目id
+      page: 1,
+      sort_type: sort_type
+    });
+
   },
   toDetailsTap: function(e) {
     var goodsId = e.currentTarget.dataset.id;
@@ -180,169 +215,7 @@ Page({
       url: "/pages/goods-details/goods-details?goodsId=" + goodsId
     })
   },
-  // tapBanner: function(e) {
-  //   if (e.currentTarget.dataset.id != 0) {
-  //     wx.navigateTo({
-  //       url: "/pages/goods-details/goods-details?id=" + e.currentTarget.dataset.id
-  //     })
-  //   }
-  // },
-  // bindTypeTap: function(e) {
-  //   this.setData({
-  //     selectCurrent: e.index
-  //   })
-  // },
-  // onLoad: function(e) {   
-  //   wx.showShareMenu({
-  //     withShareTicket: true
-  //   }) 
-  //   const that = this
-  //   // if (e && e.query && e.query.inviter_id) { 
-  //   //   wx.setStorageSync('referrer', e.query.inviter_id)
-  //   // }
-  //   if (e && e.scene) {
-  //     const scene = decodeURIComponent(e.scene)
-  //     if (scene) {        
-  //       wx.setStorageSync('referrer', scene.substring(11))
-  //     }
-  //   }
-  //   wx.setNavigationBarTitle({
-  //     title: wx.getStorageSync('mallName')
-  //   })
-  //   /**
-  //    * 示例：
-  //    * 调用接口封装方法
-  //    */
-  //   WXAPI.banners({
-  //     type: 'new'
-  //   }).then(function(res) {
-  //     if (res.code == 700) {
-  //       wx.showModal({
-  //         title: '提示',
-  //         content: '请在后台添加 banner 轮播图片，自定义类型填写 new',
-  //         showCancel: false
-  //       })
-  //     } else {
-  //       that.setData({
-  //         banners: res.data
-  //       });
-  //     }
-  //   }).catch(function(e) {
-  //     wx.showToast({
-  //       title: res.msg,
-  //       icon: 'none'
-  //     })
-  //   })
-  //   WXAPI.goodsCategory().then(function(res) {
-  //     // let categories = [{
-  //     //   id: 0,
-  //     //   icon: '/images/fl.png',
-  //     //   name: "全部"
-  //     // }];
-  //     let categories = [];
-  //     if (res.code == 0) {
-  //       categories = categories.concat(res.data)
-  //     }
-  //     const _n = Math.ceil(categories.length / 2)
-  //     // const _n = Math.ceil(categories.length)
-  //     that.setData({
-  //       categories: categories,
-  //       category_box_width: 150 * _n,
-  //       activeCategoryId: 0,
-  //       curPage: 1
-  //     });
-  //     that.getGoodsList(0);
-  //   })
-  //   WXAPI.goods({
-  //     recommendStatus: 1
-  //   }).then(res => {
-  //     if (res.code === 0){
-  //       that.setData({
-  //         goodsRecommend: res.data
-  //       })
-  //     }      
-  //   })
-  //   that.getCoupons()
-  //   that.getNotice()
-  //   that.kanjiaGoods()
-  //   that.pingtuanGoods()
-  // },
-  // onPageScroll(e) {
-  //   let scrollTop = this.data.scrollTop
-  //   this.setData({
-  //     scrollTop: e.scrollTop
-  //   })
-  // },
-  // getGoodsList: function(categoryId, append) {
-  //   if (categoryId == 0) {
-  //     categoryId = "";
-  //   }
-  //   var that = this;
-  //   wx.showLoading({
-  //     "mask": true
-  //   })
-  //   WXAPI.goods({
-  //     categoryId: categoryId,
-  //     nameLike: that.data.inputVal,
-  //     page: this.data.curPage,
-  //     pageSize: this.data.pageSize
-  //   }).then(function(res) {
-  //     wx.hideLoading()
-  //     if (res.code == 404 || res.code == 700) {
-  //       let newData = {
-  //         loadingMoreHidden: false
-  //       }
-  //       if (!append) {
-  //         newData.goods = []
-  //       }
-  //       that.setData(newData);
-  //       return
-  //     }
-  //     let goods = [];
-  //     if (append) {
-  //       goods = that.data.goods
-  //     }
-  //     for (var i = 0; i < res.data.length; i++) {
-  //       goods.push(res.data[i]);
-  //     }
-  //     that.setData({
-  //       loadingMoreHidden: true,
-  //       goods: goods,
-  //     });
-  //   })
-  // },
-  // getCoupons: function() {
-  //   var that = this;
-  //   WXAPI.coupons().then(function (res) {
-  //     if (res.code == 0) {
-  //       that.setData({
-  //         coupons: res.data
-  //       });
-  //     }
-  //   })
-  // },
-  // onShareAppMessage: function() {    
-  //   return {
-  //     title: '"' + wx.getStorageSync('mallName') + '" ' + CONFIG.shareProfile,
-  //     path: '/pages/index/index?inviter_id=' + wx.getStorageSync('uid')
-  //   }
-  // },
-  // getNotice: function() {
-  //   var that = this;
-  //   WXAPI.noticeList({pageSize: 5}).then(function (res) {
-  //     if (res.code == 0) {
-  //       that.setData({
-  //         noticeList: res.data
-  //       });
-  //     }
-  //   })
-  // },
-  // toSearch: function() {
-  //   this.setData({
-  //     curPage: 1
-  //   });
-  //   this.getGoodsList(this.data.activeCategoryId);
-  // },
+  
   onReachBottom: function() { //上滑加载更多
     wx.showLoading({
       title: '加载中...',
@@ -394,32 +267,4 @@ Page({
       inputVal: e.detail.value
     });
   },
-  // // 获取砍价商品
-  // async kanjiaGoods(){
-  //   const res = await WXAPI.goods({
-  //     kanjia: true
-  //   });
-  //   if (res.code == 0) {
-  //     this.setData({
-  //       kanjiaList: res.data
-  //     })
-  //   }
-  // },
-  // goCoupons: function (e) {
-  //   wx.navigateTo({
-  //     url: "/pages/coupons/index"
-  //   })
-  // },
-  // pingtuanGoods(){ // 获取团购商品列表
-  //   const _this = this
-  //   WXAPI.goods({
-  //     pingtuan: true
-  //   }).then(res => {
-  //     if (res.code === 0) {
-  //       _this.setData({
-  //         pingtuanList: res.data
-  //       })
-  //     }
-  //   })
-  // }
 })
